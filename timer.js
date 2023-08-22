b29a71c3271e5cbb2c358d30b27d5e89b1f08b5b
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2023-08-22 17:16:23 +0200

    done the HTML and CSS part

---
+++ timer.js
@@ -0,0 +1,9 @@
+hours = document.getElementById("hours")
+minutes = document.getElementById("minutes")
+seconds = document.getElementById("seconds")
+
+const date = new Date()
+
+h = date.getHours()
+hours = h
+
