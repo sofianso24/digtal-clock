5152709f2ba5cf04f32f305e06c2bccbba805f4d
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2023-08-22 18:25:28 +0200

    done the javaScript part an the clock works perfuctly

--- timer.js
+++ timer.js
@@ -1,9 +1,28 @@
 hours = document.getElementById("hours")
 minutes = document.getElementById("minutes")
 seconds = document.getElementById("seconds")
+ampmEl = document.getElementById("ampm")
 
-const date = new Date()
 
-h = date.getHours()
-hours = h
+const updateClock = () => {
 
+    const date = new Date()
+    let h = date.getHours()
+    let m = date.getSeconds()
+    let s = date.getMinutes()
+    let ampm = "am"
+    if (h > 12) {
+        h = h - 12
+        ampm = "pm"
+    }
+    hours.innerText = h  
+    minutes.innerText= m 
+    seconds.innerText = s 
+    ampmEl.innerText = ampm
+
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+
+updateClock()
